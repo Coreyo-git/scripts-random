bdeca49f105091204153f20d55aabde241be72f0
coreyo-git <corey.oconnor@live.com.au>
2022-06-20 16:39:16 +1000

    Updated to print output be more expendable

--- py-yt-audio-dl/py-yt-auido.py
+++ py-yt-audio-dl/py-yt-auido.py
@@ -1,8 +1,27 @@
+import os
 from pytube import Playlist
 
-# set playlist to youtube url of playlist
-pl = Playlist("https://www.youtube.com/playlist")
+songsOutputPath = './songs_output'
+songsFolderExists = os.path.exists(songsOutputPath)
+playlistURL = 'https://www.youtube.com/playlist?list=INPUT_LIST'
 
-#loop through each video and download only audio
-for video in pl.videos:
-  stream = video.streams.filter(only_audio=True).first().download('./songs_output')
+# Checks if the folder exists, crates if not
+def checkOutputExists():
+	if not songsFolderExists:
+		os.makedirs(songsOutputPath)
+
+# Downloads the playlist as audio
+def download(playlist):
+  # Tracks the download #
+  download_number = 1
+  # set playlist to youtube url of playlist
+  playlist = Playlist(playlistURL)
+  
+  #loop through each video and download only audio
+  for video in playlist.videos:
+    video.streams.filter(only_audio=True).first().download(songsOutputPath)
+    print('Video Download #',download_number, video.title)
+    download_number += 1
+    
+checkOutputExists()
+download(playlistURL)

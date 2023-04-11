4023feba5061ece3755e30c2b91d0bc6ac72bad8
coreyo-git <corey.oconnor@live.com.au>
2023-04-11 13:32:10 +1000

    updateDisc fix, bad pathing

--- updateDisc.sh
+++ updateDisc.sh
@@ -2,12 +2,12 @@
 UPDATEURL="https://discord.com/api/download?platform=linux&format=tar.gz"
 UPDATETAR="/tmp/discord.tar.gz"
 UPDATEFOLDER="/tmp/Discord"
-DISCORDPATH='/usr/lib64/discord/'
+DISCORDPATH='/usr/lib64/discord'
 
 echo "Getting discord udpate"
 
 # get latest update
-wget "$UPDATEURL" -O /tmp/discord.tar.gz
+# wget "$UPDATEURL" -O /tmp/discord.tar.gz
 
 # check if update exists
 if compgen -G "${UPDATETAR}" > /dev/null; then
@@ -17,16 +17,13 @@ if compgen -G "${UPDATETAR}" > /dev/null; then
 	# get latest update
 	# manually move update fules
 	echo "Updating..."
-	mv $UPDATEFOLDER/* $DPATH/ >/dev/null 2>&1
-	mv $UPDATEFOLDER/locales/* $DPATH/locales/ >/dev/null 2>&1
-	mv $UPDATEFOLDER/resources/* $DPATH/resources/ >/dev/null 2>&1
-	mv $UPDATEFOLDER/resources/bootstrap/* $DPATH/resources/bootstrap >/dev/null 2>&1
-	mv $UPDATEFOLDER/swiftshader/* $DPATH/swiftshader/ >/dev/null 2>&1
+	mv $UPDATEFOLDER/* $DISCORDPATH/ 
+	mv $UPDATEFOLDER/locales/* $DISCORDPATH/locales/ 
+	mv $UPDATEFOLDER/resources/* $DISCORDPATH/resources/ 
+	mv $UPDATEFOLDER/resources/bootstrap/* $DISCORDPATH/resources/bootstrap 
+	mv $UPDATEFOLDER/swiftshader/* $DISCORDPATH/swiftshader/ 
 
 	echo "Finished."
 else
 	echo "discord update could not found"
 fi
-
-	
-
